5264c9875e76a85ea54b77b758a5582d2ed6fef0
Titaniakaylife24 <titakaylife@gmail.com>
2026-07-08 18:09:16 +0700

    testing again

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
 
-echo "1<br>";
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 
 require __DIR__.'/../vendor/autoload.php';
 
-echo "2<br>";
-
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-echo "3<br>";
-exit;
+echo "BEFORE<br>";
+
+$request = Request::capture();
+
+echo "REQUEST OK<br>";
+
+$app->handleRequest($request);
+
+echo "AFTER<br>";
