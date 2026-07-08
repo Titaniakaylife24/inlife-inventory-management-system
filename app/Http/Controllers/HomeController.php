92b30b26f35377ee08ee6e38c906f3605e6f99f8
Titaniakaylife24 <titakaylife@gmail.com>
2026-07-08 14:23:41 +0700

    testing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,34 +9,7 @@ use App\Models\Location;
 class HomeController extends Controller
 {
     public function index()
-    {
-        $totalAssets = Product::count();
-
-        $borrowed = Product::where('status','Borrowed')->count();
-
-        $lowStock = Product::whereColumn(
-                'stock',
-                '<=',
-                'minimum_stock'
-            )
-            ->where('stock','>',0)
-            ->count();
-
-        $categories = Category::count();
-
-        $locations = Location::count();
-
-        $latestProducts = Product::latest()
-            ->take(3)
-            ->get();
-
-        return view('home', compact(
-            'totalAssets',
-            'borrowed',
-            'lowStock',
-            'categories',
-            'locations',
-            'latestProducts'
-        ));
-    }
+{
+    return 'HomeController OK';
+}
 }
