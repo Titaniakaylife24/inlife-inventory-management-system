f61e572894a57caed36140983a2c64795173506d
Titaniakaylife24 <titakaylife@gmail.com>
2026-07-08 19:53:17 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         View::composer('*',function($view){
+
+    if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
 
     if(Auth::check()){
 
