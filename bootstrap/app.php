087a08d257efbc2645e36be381839f8151b5ad18
Titaniakaylife24 <titakaylife@gmail.com>
2026-07-08 17:46:45 +0700

    Update bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
 
     ->withMiddleware(function (Middleware $middleware): void {
 
+    $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
